3cb674873da494ac0641c9f77261dba1a7732e32
mr5 <u@mr-5.cn>
2017-05-03 14:56:02 +0800

    fix message

--- drone-bearychat.sh
+++ drone-bearychat.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 BUILT_STATUS='Unknown'
 if [ -n "$DRONE_BUILD_STATUS" ] 
 then
@@ -24,12 +24,12 @@ curl $PLUGIN_URL \
   -H 'Content-Type: application/json' \
   -d "
 {
-  \"text\": \"${BUILT_STATUS}: build [#${DRONE_JOB_NUMBER}](${DRONE_BUILD_LINK}), in ${DRONE_COMMIT_BRANCH} of [${DRONE_REPO_NAME}](${DRONE_COMMIT_LINK})${PLUGIN_EXTERNAL_MSG}\",
+  \"text\": \"${BUILT_STATUS}: build [#${DRONE_BUILD_NUMBER}](${DRONE_BUILD_LINK}), in ${DRONE_COMMIT_BRANCH} of [${DRONE_REPO_NAME}](${DRONE_REPO_LINK})${PLUGIN_EXTERNAL_MSG}\",
   \"channel\": \"$PLUGIN_CHANNEL\",
   \"markdown\": true,
   \"attachments\": [
     {
-      \"text\": \"[${DRONE_COMMIT_REF}](${DRONE_COMMIT_LINK}) - ${DRONE_COMMIT_AUTHOR}\",
+      \"text\": \"[${DRONE_COMMIT_SHA}](${DRONE_COMMIT_LINK}) ${DRONE_COMMIT_MESSAGE} - ${DRONE_COMMIT_AUTHOR}\",
       \"color\": \"${BUILT_COLOR}\"
     }
   ]
