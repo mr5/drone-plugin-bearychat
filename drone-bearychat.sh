8b647fdcb448bdbe693ed96ebcb0a7de50fac355
mr5 <u@mr-5.cn>
2017-05-03 19:20:34 +0800

    attemp to fix DRONE_BUILD_STATUS

--- drone-bearychat.sh
+++ drone-bearychat.sh
@@ -1,22 +1,18 @@
 #!/bin/sh
 BUILT_STATUS='Unknown'
-if [ -n "$DRONE_BUILD_STATUS" ] 
-then
-  BUILT_STATUS=$DRONE_BUILD_STATUS
+if [ -n "$DRONE_BUILD_STATUS" ] ; then
+  BUILT_STATUS="$DRONE_BUILD_STATUS"
 fi
 
 BUILT_COLOR='#C9CCD1'
-if [ "$BUILT_STATUS" = 'success' ] 
-then
+if [ "$BUILT_STATUS" = 'success' ] ; then
   BUILT_COLOR='#00bfa5'
-  if [ "$BUILT_STATUS" != "$DRONE_PREV_BUILD_STATUS" ] 
-  then
+  if [ "$BUILT_STATUS" != "$DRONE_PREV_BUILD_STATUS" ] ; then
     BUILT_STATUS='Fixed'
   fi
 fi
 
-if [ "$BUILT_STATUS" = 'failure' ] 
-then
+if [ "$BUILT_STATUS" = 'failure' ] ; then
   BUILT_COLOR='#f50057'
 fi
 
@@ -24,7 +20,7 @@ curl $PLUGIN_URL \
   -H 'Content-Type: application/json' \
   -d "
 {
-  \"text\": \"${BUILT_STATUS}: build [#${DRONE_BUILD_NUMBER}](${DRONE_BUILD_LINK}), in ${DRONE_COMMIT_BRANCH} of [${DRONE_REPO_NAME}](${DRONE_REPO_LINK})${PLUGIN_EXTERNAL_MSG}\",
+  \"text\": \"*${BUILT_STATUS}*: build [#${DRONE_BUILD_NUMBER}](${DRONE_BUILD_LINK}), in ${DRONE_COMMIT_BRANCH} of [${DRONE_REPO_NAME}](${DRONE_REPO_LINK})${PLUGIN_EXTERNAL_MSG}\",
   \"channel\": \"$PLUGIN_CHANNEL\",
   \"markdown\": true,
   \"attachments\": [
